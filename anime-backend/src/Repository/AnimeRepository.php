1ae34539dbdaa2ebea2f0747d584085151e481a5
habibdevsy <alhabebcenter@hotmail.com>
2020-09-21 12:07:32 +0300

    update

--- anime-backend/src/Repository/AnimeRepository.php
+++ anime-backend/src/Repository/AnimeRepository.php
@@ -66,7 +66,7 @@ class AnimeRepository extends ServiceEntityRepository
     {
         $res = $this->createQueryBuilder('anime')
     
-        ->select('anime.id','anime.name', 'anime.id','anime.name', 'anime.mainImage','category.name as categoryName', 'count( DISTINCT interAction.type) as countInteraction', 'avg(rate.rateValue) as rating', 'count(DISTINCT comment.comment) as comments')
+        ->select('anime.id','anime.name', 'anime.id','anime.name', 'anime.mainImage','category.name as categoryName', 'count( DISTINCT interAction.type) as like', 'avg(rate.rateValue) as rating', 'count(DISTINCT comment.comment) as comments')
         
             ->leftJoin(
                 Category::class,            // Entity
