677a99c1d3ad264f160af0143fbe2a9180703306
habibdevsy <alhabebcenter@hotmail.com>
2020-10-11 20:39:23 +0300

    update unit test

--- anime-backend/src/Entity/Image.php
+++ anime-backend/src/Entity/Image.php
@@ -32,6 +32,12 @@ class Image
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
     public function getImage(): ?string
     {
         return $this->image;
