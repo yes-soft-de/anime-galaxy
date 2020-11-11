56755e7f38f468ea66f8237fb41d2e676d573919
ralsalloum <ramisalloum12@hotmail.com>
2020-11-11 19:07:30 +0200

    Behat bundle was being installed

--- anime-backend/migrations/Version20201020202811.php
+++ anime-backend/migrations/Version20201020202811.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201018082819 extends AbstractMigration
+final class Version20201020202811 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201018082819 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user_profile ADD location VARCHAR(255) DEFAULT NULL, CHANGE user_name name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user_profile ADD location VARCHAR(255) DEFAULT NULL, DROP special_link, CHANGE name user_name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user_profile DROP location, CHANGE name user_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user_profile ADD special_link TINYINT(1) DEFAULT NULL, DROP location, CHANGE user_name name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
