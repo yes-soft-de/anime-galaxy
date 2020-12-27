29c23276d1d67f9da24a73505b8922b5d9da3915
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2020-12-27 15:58:29 +0200

    change server

--- anime-flutter/anime_galaxy/lib/consts/urls.dart
+++ anime-flutter/anime_galaxy/lib/consts/urls.dart
@@ -1,5 +1,5 @@
 class Urls {
-  static const String DOMAIN = 'http://35.226.243.159';
+  static const String DOMAIN = 'http://34.71.153.58';
   static const String BASE_API = DOMAIN + '/html/public/index.php';
 
   static const String IMAGES_UPLOAD_PATH = DOMAIN +'/upload';
