4e2ed8a4536de4a805d0798984ff27e1878ed6db
ralsalloum <ramisalloum12@hotmail.com>
2021-02-08 00:17:03 +0200

    Reset Password Code was being updated

--- anime-backend/src/Manager/UserManager.php
+++ anime-backend/src/Manager/UserManager.php
@@ -175,15 +175,9 @@ class UserManager
 
     public function resetCode()
     {
-        $data = random_bytes(16);
+        $data = random_int(0, 9) . random_int(0, 9) . random_int(0, 9) . random_int(0, 9);
 
-        $data[0] = chr(ord('a') ); 
-        $data[1] = chr(ord('g') ); 
-        $data[2] = chr(ord('a') ); 
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-        return  vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        return  vsprintf('%s%s%s%s', str_split(($data)));
     }
 
 }
